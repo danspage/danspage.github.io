e0a677d04372c3669db04eff96bd3e755224fd88
Daniel  Convey <3201339@SKSD.local>
2018-05-16 16:46:52 -0400

    First commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-/** 
- * ===================================================================
- * Main js
- *
- * ------------------------------------------------------------------- 
- */ 
+/**
+* ===================================================================
+* Main js
+*
+* -------------------------------------------------------------------
+*/
 
 (function($) {
 
@@ -11,72 +11,84 @@
 
 	/* --------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
-   $(document).ready(function() {
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+	------------------------------------------------------ */
+	$(document).ready(function() {
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-
-      }); 
-  	})
+		$("#contact-link").mouseenter(function() {
+			$(this).html("[contact <i class=\"fa fa-paper-plane\" style=\"font-size:85%\"></i>]");
+		}).mouseleave(function() {
+			$(this).html("[contact]");
+		});
 
 
-  	/*---------------------------------------------------- */
+
+		$("body").on("contextmenu", "img", function(e) {
+			return false;
+		});
+
+		// Fade out the loading animation first
+		$("#loader").fadeOut("slow", function(){
+			// Fade out the black screen that covers the website
+			$("#preloader").delay(300).fadeOut("slow");
+		});
+
+	});
+
+
+	/*---------------------------------------------------- */
 	/* FitVids
-	------------------------------------------------------ */ 
-  	$(".fluid-video-wrapper").fitVids();
+	------------------------------------------------------ */
+	$(".fluid-video-wrapper").fitVids();
 
 
 	/* --------------------------------------------------- */
 	/*  Particle JS
 	------------------------------------------------------ */
 	$('.home-particles').particleground({
-	   dotColor: '#fff',
-	   lineColor: '#555555',
-	   particleRadius: 6,
-	   curveLines: true,
-	   density: 10000,
-	   proximity: 110
-	}); 
+		dotColor: '#fff',
+		lineColor: '#555555',
+		particleRadius: 6,
+		curveLines: true,
+		density: 10000,
+		proximity: 110
+	});
 
 
 	/*-----------------------------------------------------*/
 	/* tabs
-  	-------------------------------------------------------*/	
+	-------------------------------------------------------*/
 	$(".tab-content").hide();
 	$(".tab-content").first().show();
 
 	$("ul.tabs li").click(function () {
-	   $("ul.tabs li").removeClass("active");
-	   $(this).addClass("active");
-	   $(".tab-content").hide();
-	   var activeTab = $(this).attr("data-id");
-	   $("#" + activeTab).fadeIn(700);
+		$("ul.tabs li").removeClass("active");
+		$(this).addClass("active");
+		$(".tab-content").hide();
+		var activeTab = $(this).attr("data-id");
+		$("#" + activeTab).fadeIn(700);
 	});
 
 
 	/*----------------------------------------------------*/
-  	/* Smooth Scrolling
-  	------------------------------------------------------*/
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+	/* Smooth Scrolling
+	------------------------------------------------------*/
+	$('.smoothscroll').on('click', function (e) {
 
-   	var target = this.hash,
-    	$target = $(target);
+		e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+		var target = this.hash,
+		$target = $(target);
 
-  	});
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 800, 'swing', function () {
+			window.location.hash = target;
+		});
+
+	});
 
 
-  	/* --------------------------------------------------- */
+	/* --------------------------------------------------- */
 	/*  Placeholder Plugin Settings
 	------------------------------------------------------ */
 	$('input, textarea, select').placeholder();
@@ -88,18 +100,18 @@
 	var finalDate = '2018/01/01';
 
 	$('div#counter').countdown(finalDate)
-   	.on('update.countdown', function(event) {
+	.on('update.countdown', function(event) {
 
-   		$(this).html(event.strftime('<div class=\"half\">' +
-   											 '<span>%D <sup>days</sup></span>' + 
-   										 	 '<span>%H <sup>hours</sup></span>' + 
-   										 	 '</div>' +
-   										 	 '<div class=\"half\">' +
-   										 	 '<span>%M <sup>mins</sup></span>' +
-   										 	 '<span>%S <sup>secs</sup></span>' +
-   										 	 '</div>'));
+		$(this).html(event.strftime('<div class=\"half\">' +
+		'<span>%D <sup>days</sup></span>' +
+		'<span>%H <sup>hours</sup></span>' +
+		'</div>' +
+		'<div class=\"half\">' +
+		'<span>%M <sup>mins</sup></span>' +
+		'<span>%S <sup>secs</sup></span>' +
+		'</div>'));
 
-   });     
- 
+	});
+
 
 })(jQuery);
