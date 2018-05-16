74e7ced4e23907c1f6120ef2a28a247ef0598dc6
Daniel  Convey <3201339@SKSD.local>
2018-05-16 18:05:28 -0400

    Added a contact form

--- js/main.js
+++ js/main.js
@@ -115,3 +115,33 @@
 
 
 })(jQuery);
+
+
+
+
+// Email form overlay
+
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("contact-link");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+	$("#header").fadeOut(200);
+	$("#myModal").slideDown();
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	$("#myModal").slideUp();
+	$("#header").fadeIn(200);
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		$("#myModal").slideUp(function() {
+			$("#header").fadeIn(200);
+		});
+	}
+}
